d4b29e54948a17ace97a47519730adad6097de5f
leecybersec <leecybersec@gmail.com>
2020-06-19 23:53:38 +0700

    create url rule

--- export_rules/get_indicators_of_intrusion_set.py
+++ export_rules/get_indicators_of_intrusion_set.py
@@ -6,6 +6,40 @@ import os
 import json
 from pathlib import Path
 
+class Rule:
+    action = 'drop'
+    direction = '->'
+    disabled = ''
+    dst = 'any'
+    dst_port = 'any'
+    options = ''
+    content = ''
+    fast_pattern = ''
+    flow = ''
+    metadata = ''
+    msg = ''
+    reference = ''
+    rev = ''
+    sid = ''
+    protocol = ''
+    src = 'any'
+    src_port = 'any'
+
+def createRule(type_pattern, pattern):
+    rule = Rule()
+    if "url:value" in type_pattern:
+        rule.msg = rule.action + ' [' + type_pattern + '] ' + pattern
+
+        rule.protocol = "tcp"
+        rule.content = pattern.split(":")[1][2:]
+    # elif ""
+    if rule.msg != '': rule.msg = ' msg:"'+ rule.msg + '";'
+    if rule.content != '': rule.content = ' content:"'+ rule.content + '";'
+
+    final_rule = rule.action + ' ' + rule.protocol + ' ' + rule.src + ' ' + rule.src_port + ' ' + rule.direction + ' ' + rule.dst + ' ' + rule.dst_port + ' (' + rule.msg + ' ' + rule.content + ' )'
+    return final_rule
+print(createRule("url:value", "https://www.zeites.com/wp-includes/Text/Diff/Engine/native/expres.php?op=2"))
+exit()
 # Variables
 api_url = "https://projectcti.com"
 api_token = "815dd7b7-1b96-4cb1-9b0e-27d9a3d88fdb"
@@ -24,8 +58,6 @@ stix_relations = opencti_api_client.stix_relation.list(
 )
 
 print("Results: \n")
-fileName = nameIntrusionSet + ".snort"
-f = open(fileName, "w", encoding="utf-8")
 
 for stix_relation in stix_relations:
     id_pattern = stix_relation["to"]["id"]
@@ -35,8 +67,17 @@ for stix_relation in stix_relations:
         id = id_pattern
     )
 
-    print(str(indicator["indicator_pattern"]))
-    f.write(str(indicator["indicator_pattern"]))
-    f.write("\n")
+    indicator_pattern = str(indicator["indicator_pattern"])
+    type_pattern = indicator_pattern.split(" ")[0][1:]
+    pattern = indicator_pattern.split("'")[1]
 
-f.close()
+    # print(type_pattern + " : " + pattern)
+    rule = createRule(type_pattern, pattern)
+    print(rule)
+
+    # fileName = nameIntrusionSet + ".rules"
+    # f = open(fileName, "w", encoding="utf-8")
+
+    # f.write(pattern)
+    # f.write("\n")
+    # f.close()
